43c05ff38cb52d4ba9460a72e941ea5d3782bab9
bambamboole <manuel.christlieb@xentral.com>
2025-04-05 15:56:48 +0200

    Add new TranslationFile DTO

---
+++ src/DTO/TranslationFile.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace Bambamboole\LaravelLokalise\DTO;
+
+use Bambamboole\LaravelTranslationDumper\TranslationType;
+use Illuminate\Support\Str;
+
+class TranslationFile
+{
+    public function __construct(public readonly \SplFileInfo $file) {}
+
+    public function type(): TranslationType
+    {
+        return match ($this->file->getExtension()) {
+            'php' => TranslationType::PHP,
+            'json' => TranslationType::JSON,
+            default => throw new \Exception('File type not supported'),
+        };
+    }
+
+    public function locale(): string
+    {
+        $relativePath = Str::afterLast($this->file->getPathname(), '/lang/');
+
+        return match ($this->type()) {
+            TranslationType::PHP => Str::before($relativePath, '/'),
+            TranslationType::JSON => Str::before($relativePath, '.json'),
+        };
+    }
+}
