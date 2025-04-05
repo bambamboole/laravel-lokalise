58b1c7f1c0892cd451b3d531328f8ab76678034a
bambamboole <manuel.christlieb@xentral.com>
2025-04-05 15:59:02 +0200

    Add force to the upload command

--- src/Commands/UploadTranslationFilesCommand.php
+++ src/Commands/UploadTranslationFilesCommand.php
@@ -7,7 +7,7 @@ use Illuminate\Console\Command;
 
 class UploadTranslationFilesCommand extends Command
 {
-    protected $signature = 'lokalise:upload {files?*} {--cleanup}';
+    protected $signature = 'lokalise:upload {files?*} {--cleanup} {--force}';
 
     protected $description = 'Upload translations to Lokalise. This will overwrite existing translations.';
 
@@ -16,8 +16,8 @@ class UploadTranslationFilesCommand extends Command
         $files = $this->argument('files');
 
         empty($files)
-            ? $lokaliseService->uploadTranslations($this->option('cleanup'))
-            : $lokaliseService->uploadSpecificFiles($files, $this->option('cleanup'));
+            ? $lokaliseService->uploadTranslations($this->option('cleanup'), $this->option('force'))
+            : $lokaliseService->uploadSpecificFiles($files, $this->option('cleanup'), $this->option('force'));
 
         return self::SUCCESS;
     }
